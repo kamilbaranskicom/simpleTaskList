4e5f738b455cc5077d379d4d55fc2b8ec52d6e71
Kamil Barański <kamilbaranski@gmail.com>
2020-07-09 01:31:05 +0200

    immutability, new features: markAllAsDone(), hiding done tasks

--- js/taskList.js
+++ js/taskList.js
@@ -10,46 +10,78 @@
     /**
      * taskList array = [ { name: string, done: boolean }, ... ]
      */
-    const taskList = []
+    let taskList = []
 
-    const addTask = (event) => {
-        event.preventDefault();
+    const addTask = () => {
         const newTaskNameInputElement = document.querySelector('.js-newTaskName');
         const newTaskName = newTaskNameInputElement.value.trim();
 
         if (newTaskName) {
             newTaskNameInputElement.value = '';
-            taskList.push({ name: newTaskName, done: false });
+            taskList = [
+                ...taskList,
+                { name: newTaskName, done: false }
+            ];
             render();
         }
         newTaskNameInputElement.focus();
     }
 
     const deleteTask = (index) => {
-        taskList.splice(index, 1);
+        taskList = [
+            ...taskList.slice(0, index),
+            ...taskList.slice(index + 1)
+        ];
         render();
     }
 
     const toggleTaskDone = (index) => {
-        taskList[index].done = !taskList[index].done;
+        taskList = [
+            ...taskList.slice(0, index),
+            { ...taskList[index], done: !taskList[index].done },
+            ...taskList.slice(index + 1)
+        ];
+        render();
+    }
+
+    const toggleHideDone = () => {
+        const taskListElement = document.querySelector('.js-taskList');
+
+        taskListElement.classList.toggle('taskList__taskList--hideDone');
+        document.querySelector('.js-toggleHideDoneMark').innerHTML = (taskListElement.classList.contains('taskList__taskList--hideDone') ? 'Ukryj' : 'Pokaż');
+    }
+
+    const markAllAsDone = () => {
+        taskList = taskList.map(task => ({ ...task, done: true }));
         render();
     }
 
     const render = () => {
-        let HTMLString = '';
+        document.querySelector('.js-taskList').innerHTML = (taskList.map(({ done, name }) =>
+            `<li class="taskList__task${(done ? ' taskList__task--done' : '')}">
+                <button class="taskList__smallButton js-toggleTaskDone">${ (done ? '✔' : '')}</button>
+                <strong class="taskList__taskName">${name}</strong>
+                <button class="taskList__smallButton taskList__smallButton--red js-deleteTask">🗑</button>
+                `)).join('');
 
-        for (const task of taskList) {
-            HTMLString +=
-                `       <li class="taskList__task">
-            <button class="taskList__smallButton js-toggleTaskDone">${ (task.done ? '✔' : '')}</button>
-            <strong class="taskList__taskName${(task.done ? ' taskList__taskName--done' : '')}">${task.name}</strong>
-            <button class="taskList__smallButton taskList__smallButton--red js-deleteTask">🗑</button>
-        </li>
-`;
-        };
-        document.querySelector('.js-taskList').innerHTML = HTMLString;
+        setOptionalButtons();
         bindEvents();
     }
+
+    const setOptionalButtons = () => {
+        const markAllAsDoneButton = document.querySelector('.js-markAllAsDone');
+        const toggleHideDoneButton = document.querySelector('.js-toggleHideDone');
+
+        markAllAsDoneButton.disabled = taskList.every(({ done }) => done);
+
+        if (taskList.length) {
+            toggleHideDoneButton.classList.remove('taskList__textButton--hidden');
+            markAllAsDoneButton.classList.remove('taskList__textButton--hidden');
+        } else {
+            toggleHideDoneButton.classList.add('taskList__textButton--hidden');
+            markAllAsDoneButton.classList.add('taskList__textButton--hidden');
+        }
+    };
 
     const bindEvents = () => {
         document.querySelectorAll('.js-toggleTaskDone').forEach((button, index) => {
@@ -65,8 +97,10 @@
         // not needed, as we start with empty taskList, but hey, let's be solid ;)
         render();
 
-        document.querySelector('.js-addTaskForm').addEventListener('submit', addTask);
+        document.querySelector('.js-addTaskForm').addEventListener('submit', (event) => { event.preventDefault(); addTask(); });
         document.querySelector('.js-newTaskName').focus();
+        document.querySelector('.js-toggleHideDone').addEventListener('click', toggleHideDone);
+        document.querySelector('.js-markAllAsDone').addEventListener('click', markAllAsDone);
     }
 
     init();
